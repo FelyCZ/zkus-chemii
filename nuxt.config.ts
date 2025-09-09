bcc085d335d927b0e91b64bb7930c135be21eaa6
FelyCZ <jakub.feren@gmail.com>
2025-09-09 19:42:52 +0200

    feat: various nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,35 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
-    modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+    modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', ],
     css: ['~/assets/css/main.css'],
     app: {
-        pageTransition: { name: 'page', mode: 'out-in' }
+        pageTransition: {
+            name: 'page',
+            mode: 'out-in'
+        },
+        cdnURL: "https://zkus-chemii.pages.dev/"
+    },
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+            autoSubfolderIndex: false,
+            routes: [
+                "/robots.txt"
+            ],
+        },
+    },
+    ssr: true,
+    image: {
+        presets: {
+            cardImage: {
+                modifiers: {
+                    format: "jpg",
+                    width: 400,
+                    quality: 50,
+                    fit: "cover"
+                }
+            }
+        }
     },
 })
