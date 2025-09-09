a689a70b61944712e3cfcb3a9f5ea2e867909ab7
FelyCZ <jakub.feren@gmail.com>
2025-09-09 19:36:39 +0200

    feat: define card badges

---
+++ app/composables/useBadges.ts
@@ -0,0 +1,137 @@
+export default function useBadges() {
+
+    const badges = ref<CardBadge>([
+        {
+            title: "týmové",
+            icon: "i-lucide-brain",
+            color: "",
+            id: "tymove"
+        },
+        {
+            title: "individuální",
+            icon: "i-lucide-brain",
+            color: "",
+            id: "individualni"
+        },
+        {
+            title: "zima",
+            icon: "i-lucide-brain",
+            color: "",
+            id: "zima"
+        },
+        {
+            title: "korespondenční",
+            icon: "",
+            color: "",
+            id: "korespondencni"
+        },
+        {
+            title: "ve škole",
+            icon: "",
+            color: "",
+            id: "skola"
+        },
+        {
+            title: "kreativní",
+            icon: "",
+            color: "",
+            id: "kreativni"
+        },
+        {
+            title: "únikovka",
+            icon: "",
+            color: "",
+            id: "unikovka"
+        },
+        {
+            title: "stáž",
+            icon: "",
+            color: "",
+            id: "staz"
+        },
+        {
+            title: "soustředění",
+            icon: "",
+            color: "",
+            id: "soustredeni"
+        },
+        {
+            title: "léto",
+            icon: "",
+            color: "",
+            id: "leto"
+        },
+        {
+            title: "podzim",
+            icon: "",
+            color: "",
+            id: "podzim"
+        },
+        {
+            title: "tip na výlet",
+            icon: "",
+            color: "",
+            id: "tip_vylet"
+        },
+        {
+            title: "networking",
+            icon: "",
+            color: "",
+            id: "networking"
+        },
+        {
+            title: "možnosti",
+            icon: "",
+            color: "",
+            id: "moznosti"
+        },
+        {
+            title: "Praha",
+            icon: "",
+            color: "",
+            id: "praha"
+        },
+        {
+            title: "Brno",
+            icon: "",
+            color: "",
+            id: "pole"
+        },
+        {
+            title: "Pardubice",
+            icon: "",
+            color: "",
+            id: "pardubice"
+        },
+        {
+            title: "buď originál",
+            icon: "",
+            color: "",
+            id: "bud_original"
+        },
+        {
+            title: "kamkoli chceš",
+            icon: "",
+            color: "",
+            id: "kamkoli"
+        },
+    ]);
+
+    return {
+        badges
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/types/cardbadge.d.ts
@@ -0,0 +1,6 @@
+export interface CardBadge {
+    title: string;
+    icon: string;
+    color: string;
+    id: string;
+}
